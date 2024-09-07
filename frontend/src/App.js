9a47e76d26cbb3292e7d649de40568ab3fe72b53
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-06 23:09:26 -0400

    subir imagenes backend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SubirImagen from './pages/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <SubirImagen/>
+    </div>
+  );
+}
+
+export default App;
