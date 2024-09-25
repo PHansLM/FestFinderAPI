6a27f83cd61c2e8dff0085eb6dc3a0a07f08fad1
Jh0nZ <jhondeycraft776@gmail.com>
2024-09-25 09:29:24 -0400

    agregar admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+# Registra todos los modelos en el admin
+admin.site.register(Asistencia)
+admin.site.register(Consumo)
+admin.site.register(Entrada)
+admin.site.register(Establecimiento)
+admin.site.register(Etiqueta)
+admin.site.register(EtiquetaEstablecimiento)
+admin.site.register(Evento)
+admin.site.register(GeneroEvento)
+admin.site.register(Imagen)
+admin.site.register(TipoEstablecimiento)
+admin.site.register(Usuario)
+admin.site.register(ValoracionEstablecimiento)
+admin.site.register(ValoracionEvento)
+admin.site.register(Visita)
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,5 +1,15 @@
-from .Imagen import Imagen
+
+from .Asistencia import Asistencia
+from .Consumo import Consumo
+from .Entrada import Entrada 
 from .Establecimiento import Establecimiento
 from .Etiqueta import Etiqueta
-from .TipoEstablecimiento import TipoEstablecimiento
 from .EtiquetaEstablecimiento import EtiquetaEstablecimiento
+from .Evento import Evento
+from .GeneroEvento import GeneroEvento
+from .Imagen import Imagen
+from .TipoEstablecimiento import TipoEstablecimiento
+from .Usuario import Usuario
+from .ValoracionEstablecimiento import ValoracionEstablecimiento
+from .ValoracionEvento import ValoracionEvento
+from .Visita import Visita
